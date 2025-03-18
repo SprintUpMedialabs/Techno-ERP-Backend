c0baab50932d09c670a0cff06fb940219fc56a5c
Disha Modi <bhavnamodi802@gmail.com>
2025-03-18 19:25:05 +0530

    [EXtra file] : Removed extra file

--- src/finance/route/index.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { transactionRoute } from './financeRoute';
-
-export const financeRoute = express.Router();
-
-financeRoute.use('/finance', transactionRoute);
