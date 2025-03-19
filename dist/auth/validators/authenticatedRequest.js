880990f0166d527a56c1bba382b140280803ce18
drumil32 <akheniad@gmail.com>
2025-03-19 21:12:08 +0530

    build removed

--- dist/auth/validators/authenticatedRequest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserPayloadSchema = void 0;
-const zod_1 = require("zod");
-const constants_1 = require("../../config/constants");
-exports.UserPayloadSchema = zod_1.z.object({
-    id: zod_1.z.string(),
-    roles: zod_1.z.array(zod_1.z.nativeEnum(constants_1.UserRoles))
-});
