9f9cd33ba5efb8ea9fdb67700e342f0c460fb3a1
drumil32 <akheniad@gmail.com>
2025-04-08 15:12:45 +0530

    build updated

--- dist/crm/validators/leads.js
+++ dist/crm/validators/leads.js
@@ -29,15 +29,14 @@ exports.leadMasterSchema = zod_1.z.object({
 });
 exports.leadSchema = exports.leadMasterSchema.omit({
     finalConversion: true,
-    remarks: true,
     footFall: true,
     yellowLeadsFollowUpCount: true
 }).strict();
-exports.yellowLeadSchema = exports.leadMasterSchema.omit({ leadType: true, leadsFollowUpCount: true }).strict();
+exports.yellowLeadSchema = exports.leadMasterSchema.omit({ leadType: true, leadsFollowUpCount: true, leadTypeModifiedDate: true }).strict();
 exports.leadRequestSchema = exports.leadSchema.extend({
     date: commonSchema_1.requestDateSchema,
     nextDueDate: commonSchema_1.requestDateSchema.optional()
-});
+}).omit({ leadTypeModifiedDate: true });
 exports.updateLeadRequestSchema = exports.leadRequestSchema.extend({
     _id: commonSchema_1.objectIdSchema,
     date: commonSchema_1.requestDateSchema.optional(),
@@ -51,7 +50,6 @@ exports.yellowLeadUpdateSchema = exports.yellowLeadSchema.extend({
     _id: commonSchema_1.objectIdSchema,
     name: zod_1.z.string().optional(),
     phoneNumber: commonSchema_1.contactNumberSchema.optional(),
-    campusVisit: zod_1.z.boolean().optional(),
     assignedTo: commonSchema_1.objectIdSchema.optional(),
     date: commonSchema_1.requestDateSchema.transform((date) => (0, convertDateToFormatedDate_1.convertToMongoDate)(date)).optional(),
     nextDueDate: commonSchema_1.requestDateSchema.transform((date) => (0, convertDateToFormatedDate_1.convertToMongoDate)(date)).optional(),
