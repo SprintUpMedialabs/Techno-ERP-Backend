7aae3c9717b6a09e300e045d5c95b2366e255ff0
drumil32 <akheniad@gmail.com>
2025-04-24 08:10:11 +0530

    [fix]: feePaid field removed from request body

--- dist/student-data/validators/studentFilterSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.studentFilterSchema = void 0;
-const zod_1 = require("zod");
-const constants_1 = require("../../config/constants");
-exports.studentFilterSchema = zod_1.z.object({
-    course: zod_1.z.nativeEnum(constants_1.Course).optional(),
-    semester: zod_1.z.union([zod_1.z.string(), zod_1.z.number()]).optional()
-});
