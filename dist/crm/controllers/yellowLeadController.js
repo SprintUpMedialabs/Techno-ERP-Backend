31643ed63ebce3b5cd653e24f797a0c7bfabc67f
drumil32 <akheniad@gmail.com>
2025-04-26 12:08:42 +0530

    build updated

--- dist/crm/controllers/yellowLeadController.js
+++ dist/crm/controllers/yellowLeadController.js
@@ -47,7 +47,7 @@ exports.updateYellowLead = (0, express_async_handler_1.default)((req, res) => __
     }
     // If the campus visit is no, then the final conversion can not be changed.
     if (((_a = updateData.footFall) !== null && _a !== void 0 ? _a : existingLead.footFall) === false) {
-        if (updateData.finalConversion !== constants_1.FinalConversionType.NO_FOOTFALL) {
+        if (updateData.finalConversion && updateData.finalConversion !== constants_1.FinalConversionType.NO_FOOTFALL) {
             throw (0, http_errors_1.default)(400, 'Final conversion can not be changed if campus visit is no.');
         }
     }
