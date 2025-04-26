337505cb751b8518a74d3c15d912b93d0cc4b420
drumil32 <akheniad@gmail.com>
2025-04-26 11:43:03 +0530

    build updated

--- dist/index.js
+++ dist/index.js
@@ -50,14 +50,14 @@ const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 let envFile;
-if (!process.env.NODE_ENV) {
-    envFile = '.env';
-}
-else if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     envFile = '.env.prod';
 }
-else {
+else if (process.env.NODE_ENV === 'uat') {
     envFile = '.env.uat';
+}
+else {
+    envFile = '.env';
 }
 dotenv_1.default.config({ path: path_1.default.resolve(__dirname, envFile) });
 (0, validateEnv_1.validateEnvVariables)();
