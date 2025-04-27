37cdbd420be1ad7985e666c2c279a659e9cc4f40
drumil32 <akheniad@gmail.com>
2025-04-27 12:09:41 +0530

    [fix]: state enum is updated

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -73,7 +73,7 @@ export enum AdmissionReference {
   Other = "Other"
 };
 
-export enum DropDownType{
+export enum DropDownType {
   CITY = "CITY",
   FIX_CITY = "FIX_CITY",
   MAKRETING_SOURCE = "MAKRETING_SOURCE",
@@ -227,9 +227,44 @@ export enum AdmissionMode {
 
 
 export enum StatesOfIndia {
-  OTHER = "Other",
-  UttarPradesh = "Uttar Pradesh",
+  ANDHRA_PRADESH = "Andhra Pradesh",
+  ARUNACHAL_PRADESH = "Arunachal Pradesh",
+  ASSAM = "Assam",
+  BIHAR = "Bihar",
+  CHHATTISGARH = "Chhattisgarh",
+  GOA = "Goa",
+  GUJARAT = "Gujarat",
+  HARYANA = "Haryana",
+  HIMACHAL_PRADESH = "Himachal Pradesh",
+  JHARKHAND = "Jharkhand",
+  KARNATAKA = "Karnataka",
+  KERALA = "Kerala",
+  MADHYA_PRADESH = "Madhya Pradesh",
+  MAHARASHTRA = "Maharashtra",
+  MANIPUR = "Manipur",
+  MEGHALAYA = "Meghalaya",
+  MIZORAM = "Mizoram",
+  NAGALAND = "Nagaland",
+  ODISHA = "Odisha",
+  PUNJAB = "Punjab",
+  RAJASTHAN = "Rajasthan",
+  SIKKIM = "Sikkim",
+  TAMIL_NADU = "Tamil Nadu",
+  TELANGANA = "Telangana",
+  TRIPURA = "Tripura",
+  UTTAR_PRADESH = "Uttar Pradesh",
+  UTTARAKHAND = "Uttarakhand",
+  WEST_BENGAL = "West Bengal",
+  ANDAMAN_AND_NICOBAR_ISLANDS = "Andaman and Nicobar Islands",
+  CHANDIGARH = "Chandigarh",
+  DADRA_AND_NAGAR_HAVELI_AND_DAMAN_AND_DIU = "Dadra and Nagar Haveli and Daman and Diu",
+  DELHI = "Delhi",
+  JAMMU_AND_KASHMIR = "Jammu and Kashmir",
+  LADAKH = "Ladakh",
+  LAKSHADWEEP = "Lakshadweep",
+  PUDUCHERRY = "Puducherry"
 }
+
 
 export enum Countries {
   India = "India",
@@ -245,7 +280,7 @@ export enum Countries {
 //   Unnao = "Unnao",
 // }
 
-export enum AreaType{
+export enum AreaType {
   URBAN = "URBAN",
   RURAL = "RURAL",
   OTHERS = "OTHERS"
@@ -280,7 +315,7 @@ export enum COLLECTION_NAMES {
 }
 
 
-export enum LectureConfirmation{
+export enum LectureConfirmation {
   CONFIRMED = 'CONFIRMED',
   DELAYED = 'DELAYED',
   TO_BE_DONE = 'TO_BE_DONE'
@@ -293,7 +328,7 @@ export enum RequestAction {
   PATCH = "PATCH",
 }
 
-export enum CourseMaterialType{
+export enum CourseMaterialType {
   LPLAN = 'LPlan',
   PPLAN = 'PPlan',
   GENERAL = 'General'
