5076d14b313027d16b7eb7a0a829b0deb5ab4b1f
drumil32 <akheniad@gmail.com>
2025-05-06 08:36:48 +0530

    .gitignore updated

--- dist/admission/validators/enquiryIdMetaDataSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.enquiryIdMetaDataSchema = void 0;
-const zod_1 = require("zod");
-const constants_1 = require("../../config/constants");
-exports.enquiryIdMetaDataSchema = zod_1.z.object({
-    prefix: zod_1.z.nativeEnum(constants_1.FormNoPrefixes),
-    lastSerialNumber: zod_1.z.number().int().min(0),
-});
