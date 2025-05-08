f6d39ba944b97a33309356b34f84e6160d70dd47
drumil32 <akheniad@gmail.com>
2025-05-08 17:43:20 +0530

    [fix]: minor changes

--- src/admission/controllers/enquiryStep3Controller.ts
+++ src/admission/controllers/enquiryStep3Controller.ts
@@ -21,7 +21,6 @@ export const saveStep3Draft = expressAsyncHandler(functionLevelLogger(async (req
   if (!validation.success) {
     throw createHttpError(400, validation.error.errors[0]);
   }
-
   const { id, ...validatedData } = validation.data;
 
   const isEnquiryExists = await Enquiry.exists({
