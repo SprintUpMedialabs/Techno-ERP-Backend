af03dfcfce44e883c32bc746554982e765d461cb
drumil32 <akheniad@gmail.com>
2025-05-23 16:18:42 +0530

    [fix]: build error

--- src/auth/utils/getCurrentLoggedInUser.ts
+++ src/auth/utils/getCurrentLoggedInUser.ts
@@ -1,8 +1,8 @@
-import { verifyToken } from "../../utils/jwtHelper";
 import { AuthenticatedRequest } from "../validators/authenticatedRequest";
+import { studentJwtHelper } from "../../utils/jwtHelper";
 
 export const getCurrentLoggedInUser = ( req : AuthenticatedRequest ) => {
     const token = req.cookies.token;
-    const decoded = verifyToken(token) as { id: string };
+    const decoded = studentJwtHelper.verifyToken(token) as { id: string };
     return decoded.id;
 }
