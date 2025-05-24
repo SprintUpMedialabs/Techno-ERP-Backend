5fcc26f967800cfc5d3de22f992cc66857d4a7c1
drumil32 <akheniad@gmail.com>
2025-05-25 00:12:22 +0530

    [fix]: admission analytics

--- src/admission/routes/index.ts
+++ src/admission/routes/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import { enquiryRoute } from './enquiryFormRoute';
-import { getAdmissionStats } from '../controllers/admissionController';
+import { getAdmissionStats } from '../controllers/admissionAnalyticsController';
 import { UserRoles } from '../../config/constants';
 import { authenticate, authorize } from '../../middleware/jwtAuthenticationMiddleware';
 export const admissionRoute = express.Router();
 
 admissionRoute.use('/enquiry', enquiryRoute);
 
-admissionRoute.get('/admission-analytics',
+admissionRoute.get('/analytics',
     authenticate,
     authorize([UserRoles.BASIC_USER]),
     getAdmissionStats
