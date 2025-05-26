9f26709d7284e3fe4aad5ce2bd9860f2c3110fdf
drumil32 <akheniad@gmail.com>
2025-05-26 22:13:12 +0530

    build updated

--- dist/utils/getISTDate.js
+++ dist/utils/getISTDate.js
@@ -1,10 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getISTDate = getISTDate;
+const moment_timezone_1 = __importDefault(require("moment-timezone"));
 function getISTDate(offsetDays = 0) {
-    const now = new Date();
-    const ist = new Date(now.getTime() + 330 * 60000);
-    ist.setDate(ist.getDate() + offsetDays);
-    ist.setHours(0, 0, 0, 0);
+    const now = (0, moment_timezone_1.default)().tz('Asia/Kolkata');
+    const ist = now.clone().add(offsetDays, 'days').startOf('day').toDate();
     return ist;
 }
